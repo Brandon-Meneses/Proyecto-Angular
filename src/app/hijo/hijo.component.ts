cde7bb7b5f1e3f35a9794111b79b28f3078644e0
Brandon-Meneses <134113321+Brandon-Meneses@users.noreply.github.com>
2023-12-21 01:47:37 -0500

    Hijo manda instruccion por boton  y padre realiza la logica

--- src/app/hijo/hijo.component.ts
+++ src/app/hijo/hijo.component.ts
@@ -7,10 +7,18 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class HijoComponent {
   @Output() mensajeHijo= new EventEmitter<string>();
+  @Output() incrementarHijo= new EventEmitter<string>();
+  @Output() decrementarHijo= new EventEmitter<string>();
 
   mensaje: string =''; //variable que se va a enviar al padre
 
   enviarMensaje() {
     this.mensajeHijo.emit(this.mensaje);
+  }
+  incrementar() {
+    this.incrementarHijo.emit();
+  }
+  decrementar() {
+    this.decrementarHijo.emit();
   }
 }
